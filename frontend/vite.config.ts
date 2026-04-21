96f629e2db512eb37809b9b50af6018bdf143905
Sergionx <sergio180203@gmail.com>
2026-04-20 22:03:31 -0400

    refactor: remove Netlify plugin and update routing logic
    - Removed the Netlify Vite plugin from package.json and vite.config.ts.
    - Updated the main route in index.tsx to redirect to "/notes" instead of rendering the initial app component.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 import { nitro } from "nitro/vite"
-import netlify from "@netlify/vite-plugin-tanstack-start"
 
 const config = defineConfig({
   plugins: [
@@ -13,7 +12,6 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
-    netlify(),
   ],
   resolve: {
     tsconfigPaths: true,
